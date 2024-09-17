8974f4a971dae074f2af11ca210bd1a96a6fc1cf
Merenciou <vitor.merencio.2012@hotmail.com>
2024-09-17 19:50:29 -0300

    navbar mobile done

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,51 +1,125 @@
+import { useState } from 'react';
 import './Navbar.css';
 
 function Navbar (){
+
+    const [burger_class, setBurgerClass] = useState('burger-bar unclicked');
+    const [menu_class, setMenuClass] = useState('menu hidden');
+    const [isMenuClicked, setIsMenuClicked] = useState(false);
+
+    const updateMenu = () => {
+        if(!isMenuClicked){
+            setBurgerClass('burger-bar clicked');
+            setMenuClass('menu visible');
+        } else {
+            setBurgerClass('burger-bar unclicked');
+            setMenuClass('menu hidden');
+        }
+        setIsMenuClicked(!isMenuClicked);
+    }
+
+    
+
     return (
-        <nav className='navbarDesktop'>
-            <div className='containerOne'>
-                <ul>
-                    <h1>
-                        <span className="logoName">Vitor </span> 
-                        <span className="logoLastName">Merencio</span>
-                    </h1>
-                </ul>
-                
-                
-            </div>
-            
-            <div className='containerTwo'>
-            <ul className='navDesktop'>
-                <li className='navItem'>
-                    <a href="#homePage" className='navText' >Página Inicial</a>
-                </li>
-            </ul>
-            <ul className='navDesktop'>
-                <li className='navItem'>
-                    <a href="#aboutPage" className='navText' >Sobre</a>
-                </li>
-            </ul>
-            <ul className='navDesktop'>
-                <li className='navItem'>
-                    <a href="#projectsPage" className='navText' >Projetos</a>
-                </li>
-            </ul>
-            <ul className='navDesktop'>
-                <li className='navItem'>
-                    <a href="#knowledgesPage" className='navText' >Conhecimentos</a>
-                </li>
-            </ul>
-            <ul className='navDesktop'>
-                <li className='navItem'>
-                    <a href="#contactPage" className='navText' >Contato</a>
-                </li>
-            </ul>
+        <div>
+            <nav className='navbarDesktop'>
+                <div className='containerOne'>
+                    <ul>
+                        <h1>
+                            <span className="logoName">Vitor </span> 
+                            <span className="logoLastName">Merencio</span>
+                        </h1>
+                    </ul>          
+                </div>        
+                <div className='containerTwo'>
+                    <ul className='navDesktop'>
+                        <li className='navItem'>
+                            <a href="#homePage" className='navText' >Página Inicial</a>
+                        </li>
+                    </ul>
+                    <ul className='navDesktop'>
+                        <li className='navItem'>
+                            <a href="#aboutPage" className='navText' >Sobre</a>
+                        </li>
+                    </ul>
+                    <ul className='navDesktop'>
+                        <li className='navItem'>
+                            <a href="#projectsPage" className='navText' >Projetos</a>
+                        </li>
+                    </ul>
+                    <ul className='navDesktop'>
+                        <li className='navItem'>
+                            <a href="#knowledgesPage" className='navText' >Conhecimentos</a>
+                        </li>
+                    </ul>
+                    <ul className='navDesktop'>
+                        <li className='navItem'>
+                            <a href="#contactPage" className='navText' >Contato</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            <div className='coverMenu'>
 
+
+
+
+
+
+
+                <nav className='navMobile' >
+                    <div className='burger-menu' onClick={updateMenu}>
+                        <div className={burger_class}></div>
+                        <div className={burger_class}></div>
+                        <div className={burger_class}></div>
+                    </div>
+                </nav>
+
+                <div className={menu_class}>
+
+                    <div className='menuItems'>
+                        <ul className='navDesktop'>
+                            <li className='navItem'>
+                                <a href="#homePage" className='navText' >Página Inicial</a>
+                            </li>
+                        </ul>
+                        <ul className='navDesktop'>
+                            <li className='navItem'>
+                                <a href="#aboutPage" className='navText' >Sobre</a>
+                            </li>
+                        </ul>
+                        <ul className='navDesktop'>
+                            <li className='navItem'>
+                                <a href="#projectsPage" className='navText' >Projetos</a>
+                            </li>
+                        </ul>
+                        <ul className='navDesktop'>
+                            <li className='navItem'>
+                                <a href="#knowledgesPage" className='navText' >Conhecimentos</a>
+                            </li>
+                        </ul>
+                        <ul className='navDesktop'>
+                            <li className='navItem'>
+                                <a href="#contactPage" className='navText' >Contato</a>
+                            </li>
+                        </ul>
+                    </div>
+
+
+
+
+
+
+
+
+
+                    
+
+                </div>
             </div>
-            
-        </nav>
+
+        </div>
         
-
     )
 }
 
