37bf0480d4ebf6c7575656067499879552375946
Merenciou <vitor.merencio.2012@hotmail.com>
2024-09-17 20:39:42 -0300

    homepage reponsiveness done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,7 +6,10 @@ function Home() {
             <section id="homePage">
                 <div className='mainContainerHome'>
                     <div className='containerPrimaryHome'>
-                        <h1 className='homepageTitle'>Vitor<br/> Merencio</h1>
+                        <h1 className='homepageTitle'>
+                            <span>Vitor </span>
+                            <span>Merencio</span>
+                             </h1>
                         <h2 className='homepageSubtitle'>Desenvolvedor de Software</h2>
                     </div>
                     <div className='containerSecondaryHome'>
